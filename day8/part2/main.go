f56e5d8431103f07ef6075c4d491bd2dbee71635
Jeff Martin <jeff.martin@getweave.com@jeffs-MBP.hsd1.ut.comcast.net>
2019-12-21 08:55:37 -0700

    fixing folder structure

---
+++ day8/part2/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var wide = 25
+var tall = 6
+
+type layer struct {
+	data [][]int
+}
+
+func (l *layer) numDigit(digit int) int {
+	var numDigit int
+	for _, row := range l.data {
+		for _, v := range row {
+			if v == digit {
+				numDigit++
+			}
+		}
+	}
+	return numDigit
+}
+
+func (l *layer) print() {
+	for _, row := range l.data {
+		for _, v := range row {
+			fmt.Print(v)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func (l *layer) printColor() {
+	for _, row := range l.data {
+		for _, v := range row {
+			c := "#" // white
+			if v == 0 {
+				c = " " // black
+			}
+			fmt.Print(c)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+// 0 is black, 1 is white, and 2 is transparent.
+func main() {
+	layers := readLayers()
+
+	answer := &layer{data: make([][]int, 0)}
+	for j := 0; j < tall; j++ {
+		answer.data = append(answer.data, make([]int, wide))
+	}
+
+	for i := 0; i < tall; i++ {
+		for j := 0; j < wide; j++ {
+			// find the first pixel that is not a 2 (transparent)
+			for _, layer := range layers {
+				layer.print()
+				v := layer.data[i][j]
+				if v != 2 {
+					answer.data[i][j] = v
+					break
+				}
+			}
+		}
+	}
+
+	answer.printColor()
+}
+
+func readLayers() []*layer {
+	file, err := os.Open("/Users/jeff.martin@getweave.com/Documents/advent-of-code/day8/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var input []string
+	for scanner.Scan() {
+		l := scanner.Text()
+		input = strings.Split(l, "")
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	intArray := make([]int, 0)
+	for _, v := range input {
+		i, _ := strconv.Atoi(v)
+		intArray = append(intArray, i)
+	}
+
+	layers := make([]*layer, 0)
+	l := &layer{data: make([][]int, 0)}
+	for i := 0; i < len(intArray); i += wide {
+		row := intArray[i : i+wide]
+		if len(l.data) < tall {
+			l.data = append(l.data, row)
+		} else {
+			layers = append(layers, l)
+			l = &layer{data: make([][]int, 0)}
+			l.data = append(l.data, row)
+		}
+	}
+	layers = append(layers, l)
+
+	return layers
+}
